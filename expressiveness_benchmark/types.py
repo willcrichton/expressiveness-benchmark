6c62f7bb1f57038d2b57d4ae0b4a784828490aa3
Will Crichton <wcrichto@cs.stanford.edu>
2020-10-27 18:35:02 -0700

    Add more tasks

--- expressiveness_benchmark/types.py
+++ expressiveness_benchmark/types.py
@@ -73,6 +73,37 @@ class Program(Base):
             DATA_DIR, "programs", f"{self.language}_{self.task}_{self.author}.json"
         )
 
+    def to_dataframe(self, value):
+        if isinstance(value, pd.DataFrame):
+            return value
+
+        if isinstance(value, list):
+            if len(value) > 0 and isinstance(value[0], dict):
+                return pd.DataFrame(value)
+            else:
+                return pd.DataFrame([[el] for el in value])
+        else:
+            return pd.DataFrame([value])
+
+    def check_equals(self, target, actual):
+        target = self.to_dataframe(target)
+        actual = self.to_dataframe(actual)
+
+        if len(target) != len(actual):
+            print("Mismatch between target and actual output.")
+            print("Target: ", target)
+            print("Actual: ", actual)
+            assert False
+
+        target = target.sort_values(by=target.columns[0], ignore_index=True)
+        actual = actual.sort_values(by=target.columns[0], ignore_index=True)
+
+        if not target.equals(actual):
+            print("Mismatch between target and actual output.")
+            print("Target: ", target)
+            print("Actual: ", actual)
+            assert False
+
     def execute(self, task):
         dataframes = {}
         for table_name, table in task.sample_input.items():
@@ -85,7 +116,6 @@ class Program(Base):
 
             call = f"{task.id}(**{task.sample_input})"
             ret = eval(call, globls, globls)
-            assert ret == task.sample_output
 
         elif self.language == "sql":
             conn = sqlite3.connect(":memory:")
@@ -95,8 +125,9 @@ class Program(Base):
                 conn.commit()
                 c = conn.cursor()
                 c.execute(self.source)
-                ret = c.fetchone()
-                assert ret[0] == task.sample_output
+                ret = [list(r) for r in c.fetchall()]
+                if len(ret) > 0 and len(ret[0]) == 1:
+                    ret = [r[0] for r in ret]
             finally:
                 conn.close()
 
@@ -128,10 +159,23 @@ class Program(Base):
                         header=False,
                     )
 
-                sp.check_call("souffle -F. -D. program.dl", cwd=path, shell=True)
+                try:
+                    sp.check_output(
+                        "souffle -F. -D. program.dl",
+                        cwd=path,
+                        shell=True,
+                        stderr=sp.PIPE,
+                    )
+                except sp.CalledProcessError as e:
+                    print(e.stderr.decode("utf-8"))
+                    raise
 
-                output = pd.read_csv(f"{path}/{task.id}.csv", header=None)
-                assert output[0][0] == task.sample_output
+                try:
+                    ret = pd.read_csv(f"{path}/{task.id}.csv", header=None)
+                except pd.errors.EmptyDataError:
+                    ret = pd.DataFrame()
+
+        self.check_equals(task.sample_output, ret)
 
 
 def load_all_programs():
@@ -163,7 +207,40 @@ TASKS = {
                 ]
             },
             sample_output="Mary",
-        )
+        ),
+        Task(
+            id="continent_by_population",
+            description="Find the continent with the highest average population",
+            plan=[],
+            sample_input={
+                "countries": [
+                    {"name": "USA", "population": 328, "continent": "North America"},
+                    {"name": "Canada", "population": 37, "continent": "North America"},
+                    {"name": "Ethiopia", "population": 109, "continent": "Africa"},
+                    {"name": "Kenya", "population": 51, "continent": "Africa"},
+                ]
+            },
+            sample_output="North America",
+        ),
+        Task(
+            id="unique_beer_drinkers",
+            description="Find the people who like a unique set of beer",
+            plan=[],
+            sample_input={
+                "likes": [
+                    {"name": "will", "beer": "ipa"},
+                    {"name": "will", "beer": "lager"},
+                    {"name": "scott", "beer": "ipa"},
+                    {"name": "scott", "beer": "stout"},
+                    {"name": "gleb", "beer": "ipa"},
+                    {"name": "gleb", "beer": "stout"},
+                    {"name": "fred", "beer": "ipa"},
+                    {"name": "fred", "beer": "lager"},
+                    {"name": "fred", "beer": "stout"},
+                ]
+            },
+            sample_output=["will", "fred"],
+        ),
     ]
 }
 
