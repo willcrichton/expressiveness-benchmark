67a72d118aab9da44945daf831f2f1e30539049d
Will Crichton <wcrichto@cs.stanford.edu>
2020-12-30 16:50:01 -0500

    Add better line height

--- code_widget/src/editor.tsx
+++ code_widget/src/editor.tsx
@@ -198,7 +198,7 @@ export let CodeViewer = observer((props: CodeViewerProps) => {
       />
     </div>
     {!editor_init ? <pre style={{
-      width, height, lineHeight: '1em',
+      width, height, lineHeight: '1.3em',
       fontSize: editor_props ? editor_props.fontSize : null
     }}>{program.source}</pre> : null}
   </div>);
